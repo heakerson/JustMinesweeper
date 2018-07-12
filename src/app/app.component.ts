cb73b8316c951650633dbfc3cb624159179da932
Heather Akerson <HeatherA@Magenic.com>
2018-07-12 12:43:32 -0500

    Stopped game when stats modal is opened

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { BoardComponent } from './board/board.component';
+import { GameStatus } from './board/GameStatus';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  
+  ChildComponent : BoardComponent;
+
+  onActivate(comp){
+      this.ChildComponent = <BoardComponent>comp;
+  }
+
+  PauseGame(){
+    if(this.ChildComponent instanceof BoardComponent){
+      this.ChildComponent.Model.Stop();
+    }
+  }
+
+  StartGame(){
+    if(this.ChildComponent instanceof BoardComponent){
+      if(this.ChildComponent.Model.GameStatus == GameStatus.Stopped){
+        this.ChildComponent.Model.Start();
+      }
+    }
+  }
+
 }
