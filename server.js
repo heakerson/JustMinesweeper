ca1e4ba53328ba847c86818cbf4817026d97179a
Heather Akerson <HeatherA@magenic.com>
2018-09-05 16:12:06 -0500

    minor fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/Minesweeper')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
